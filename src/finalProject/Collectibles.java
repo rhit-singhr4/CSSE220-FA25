460ca79034d36ff1aea79f2c8e1cdf3d2ba7338b
Andrew Noblitt <noblitaj@rose-hulman.edu>
2025-11-03 14:00:45 -0500

    Andrew created Collectibles class

---
+++ src/finalProject/Collectibles.java
@@ -0,0 +1,6 @@
+package finalProject;
+
+public class Collectibles {
+
+	
+}
--- src/finalProject/Game.java
+++ src/finalProject/Game.java
@@ -1,73 +1,53 @@
 package finalProject;
-
-import java.awt.Color;
+ 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.io.IOException;
-import java.util.Random;
-
-import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-
+import javax.swing.Timer;
+ 
 public class Game extends JComponent{
-	public static final Color BG = Color.CYAN;
-	private Image cloud;
-	private boolean spriteLoaded = false;
-	
+ 
 	private Player player = new Player();
-	Inputs inputs = new Inputs(player, this);
-
+	private Enemy enemy1 = new Enemy();
+	private Inputs inputs = new Inputs(player);
+	private Platforms platforms = new Platforms();
+	private JFrame frame = new JFrame();
+	private Timer timer;
 	
 	public void show() {
-		JFrame frame = new JFrame();
 		frame.add(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1920,1080);
+		frame.setSize(960,540);
 		frame.setVisible(true);
-		
-		
 		this.setFocusable(true);
 		this.addKeyListener(inputs);
+		startGameLoop();
 	}
+	
+    private void startGameLoop() {
+        timer = new Timer(1000 / 60, e -> {
+            player.update();
+            inputs.update();
+            player.updatePlayerSprite();
+            enemy1.update();
+            repaint();
+        });
+        timer.start();
+    }
 	
 	public Game() {
 	}
 	
-	
-	
-	
-
 	@Override
 	protected void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paintComponent(g);
-		g.setColor(BG);
+//		g.setColor(BG);
 		g.fillRect(0,  0,  getWidth(), getHeight());
-		try {
-			cloud = ImageIO.read(getClass().getResource("cloud.png"));
-			spriteLoaded = true;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		if(spriteLoaded) {
-			Random random = new Random();
-			int x = 0;
-			int y = 0;
-			g.drawImage(cloud, x, y, 150, 150, null);
-			for (int i = 0; i < 9; i++) {
-				x = 150 + x;
-				y = 100 - random.nextInt(200);
-				g.drawImage(cloud, x, y, 150, 150, null);
-				
-			}
-		}
+		platforms.draw(g,getWidth(),getHeight());
+		enemy1.draw(g);
 		player.draw(g);
-	
-		
-		
 	}
 }
+ 
+ 
