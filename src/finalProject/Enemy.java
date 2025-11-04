8ef2dc6b3e4e0645d71f56eadafc1f9351bff952
singhr4 <singhr4@rose-hulman.edu>
2025-11-04 14:40:46 -0500

    Raj - Working on enemies

--- src/finalProject/Enemy.java
+++ src/finalProject/Enemy.java
@@ -12,62 +12,75 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 
-public class Enemy{
-	
+public class Enemy {
+
+	private Image enemy;
 	private boolean facingLeft = false;
 	private static final int SPEED = 3;
 	private static final int SPRITE_WIDTH = 35;
 	private static final int SPRITE_HEIGHT = 35;
 
-	
-	private int x = 100;
-	private int y = 200;
-	
+	private int x;
+	private int y;
+
 	private SpriteManager sprites = new SpriteManager();
-	
-	
 	public Enemy() {
-		sprites.loadEnemy();
+		this.x = 100;
+		this.y = 200;
+		this.enemy = enemy;
 	}
 	
 	public void update() {
-		if(facingLeft) {
+		if (facingLeft) {
 			moveLeft();
 		} else {
 			moveRight();
 		}
-		
-		if(x <= 0) {
+
+		if (x <= 0) {
 			moveRight();
-		}
-		
-		if(x + SPRITE_WIDTH >= 1920) {
+		} else if (x + SPRITE_WIDTH >= 1460) {
 			moveLeft();
 		}
+		
 		sprites.updateEnemy1Sprite(facingLeft);
 	}
-	
+
 	public void moveLeft() {
 		x -= SPEED;
 		facingLeft = true;
 	}
-	
+
 	public void moveRight() {
 		x += SPEED;
 		facingLeft = false;
 	}
-	
+
 	public void draw(Graphics g) {
-		Image currentImage = sprites.enemyImage();
-		g.drawImage(currentImage, x, y, SPRITE_WIDTH, SPRITE_HEIGHT, null);
-		
+		g.drawImage(enemy, x, y, SPRITE_WIDTH, SPRITE_HEIGHT, null);
+
 		g.setColor(Color.BLACK);
 		g.drawRect(x, y, SPRITE_WIDTH, SPRITE_HEIGHT);
 	}
-	
-	public void collide() {
-		
+
+	public static int getSpriteWidth() {
+		return SPRITE_WIDTH;
 	}
-	
-	
+
+	public static int getSpriteHeight() {
+		return SPRITE_HEIGHT;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public boolean getIsFacingLeft() {
+		return facingLeft;
+	}
+
 }
--- src/finalProject/EnemyManager.java
+++ src/finalProject/EnemyManager.java
@@ -1,5 +1,6 @@
 package finalProject;
 
+import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class EnemyManager {
@@ -9,4 +10,25 @@ public class EnemyManager {
 	private static final int SPRITE_WIDTH = 35;
 	private static final int SPRITE_HEIGHT = 35;
 	
+	public EnemyManager() {
+		sprite.loadEnemy();
+	}
+	
+	public void addEnemies(Platform platform) {
+		int enemyX = platform.getX() + 10;
+		int enemyY = platform.getY() - SPRITE_HEIGHT;
+		enemies.add(new Enemy(enemyX, enemyY));
+	}
+	
+	public void update() {
+		for(Enemy enemy: enemies) {
+			enemy.update();
+		}
+	}
+	
+	public void draw(Graphics g) {
+		for(Enemy enemy: enemies) {
+			enemy.draw(g);
+		}
+	}
 }
